b17883f81416a9d7c351750f39bcf87cc9a7ea23
Tadej Glinšek <tadej.glinsek@gmail.com>
2022-10-06 14:55:26 +0200

    Mape in podmape

--- seznamPesmi.js
+++ seznamPesmi.js
@@ -1,6 +1,8 @@
-var seznamPesmi = [
-  "prva_pesem",
-  "druga_pesem",
-  "tretja_pesem",
-  "dodatne_pesmi/četrta_pesem"
-];
+var seznamPesmi = {
+  "prva_pesem" : null,
+  "druga_pesem" : null,
+  "tretja_pesem" : null,
+  "dodatne_pesmi" : {
+    "četrta_pesem" : null
+  }
+};
